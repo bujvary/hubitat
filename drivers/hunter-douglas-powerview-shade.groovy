60f05c2fcbc0785414995a1002b1ab83a6d0aaca
Brian Ujvary <40494729+bujvary@users.noreply.github.com>
2021-06-07 09:47:36 -0800

    Update hunter-douglas-powerview-shade.groovy

--- drivers/hunter-douglas-powerview-shade.groovy
+++ drivers/hunter-douglas-powerview-shade.groovy
@@ -15,6 +15,11 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  *  Change Log:
+ *    04/25/2021 v1.2 - Refactored sendEvent() for battery and shade levels
+ *    03/28/2021 v1.1 - Added firmware version and shade type to the Data section of Device Details
+ *                    - Changed battery percentage calculation based on information from Hunter Douglas engineers
+ *                    - Added "position" event for WindowShade capability
+ *                    - Modified code to use the "capabilities" value of shade and added logic to handle various shade types properly
  *    05/10/2020 v1.0 - Initial release
  *
  */
@@ -24,8 +29,8 @@ metadata {
         capability "Battery"
         capability "Refresh"
         capability "Sensor"
-        capability "Switch Level"
         capability "Window Shade"
+		capability "Switch Level"
 
         command "calibrate"
         command "jog"
@@ -37,7 +42,8 @@ metadata {
     }
 
     preferences {
-        input("logEnable", "bool", title: "Enable logging", required: false, defaultValue: true)
+        input name: 'maxVoltage', type: 'number', title: '<b>Maximum Voltage</b>', description: '<div><i>Maximum voltage of battery wand</i></div><br>', defaultValue: '18'
+        input name: 'logEnable', type: 'bool', title: '<b>Enable Logging?</b>', description: '<div><i>Automatically disables after 15 minutes.</i></div><br>', defaultValue: true
     }
 }
 
@@ -47,6 +53,83 @@ def installed() {
 
 def initialize() {
     if (logEnable) runIn(900, logsOff)
+    
+    if (!state."Shade capability information from Hunter Douglas") {
+      state."Shade capability information from Hunter Douglas" = '''
+<style type="text/css">
+  .tg  {border-collapse:collapse;border-color:#ccc;border-spacing:0;font-weight:normal;}
+  .tg td{background-color:#fff;border-color:#ccc;border-style:solid;border-width:1px;color:#333;
+    font-family:Helvetica,Arial,sans-serif;font-size:14px;overflow:hidden;padding:4px 6px;word-break:normal;}
+  .tg th{background-color:#f0f0f0;border-color:#ccc;border-style:solid;border-width:1px;color:#333;
+    font-family:Helvetica,Arial,sans-serif;font-size:14px;overflow:hidden;padding:4px 6px;word-break:normal;}
+  .tg .tg-1wig{font-weight:bold;text-align:left;vertical-align:top;}
+  .tg .tg-baqh{text-align:center;vertical-align:top;width:110px;}
+  .tg .tg-02ax{text-align:left;vertical-align:top;width:145px;}
+  .tg .tg-01ax{text-align:left;vertical-align:top;}
+</style>
+
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-1wig">Capability Value</th>
+    <th class="tg-1wig">Shade Type</th>
+    <th class="tg-1wig">Shade Description</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-baqh">0</td>
+    <td class="tg-02ax">Bottom Up</td>
+    <td class="tg-0lax">Shades with standard bottom-up operation such as Alustra Woven Textures, Roller, Screen &amp; Banded Shades, Duette/Applause Standard, Design Studio shades, Solera, Vignette Modern Roman Shades Provenance Woven Wood Shades.</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">1</td>
+    <td class="tg-02ax">Bottom Up Tilt 90°</td>
+    <td class="tg-0lax">Shades with Bottom-Up lift and 90° Tilt. Includes Pirouette, Silhouette, Silhouette A Deux Front Shade.</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">2</td>
+    <td class="tg-02ax">Bottom Up Tilt 180°</td>
+    <td class="tg-0lax">Lift and Tilt Horizontal Blind (Not sold in USA at this time).</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">3</td>
+    <td class="tg-02ax">Vertical</td>
+    <td class="tg-0lax">Vertically oriented shades with horizontal traverse operation only. Include Skyline Left Stack, Right Stack, Split Stack; Provenance Vertical Drapery Left Stack, Right Stack, Split Stack; Duette/Applause Vertiglide Left Stack, Right Stack.</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">4</td>
+    <td class="tg-02ax">Vertical Tilt 180°</td>
+    <td class="tg-0lax">Vertically oriented shades with horizontal traverse operation plus 180° Tilt. Includes Luminette Left Stack, Right Stack and Split Stack.</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">5</td>
+    <td class="tg-02ax">Tilt Only 180°</td>
+    <td class="tg-0lax">Products with tilt-only operation such as Parkland, EverWood, Modern Precious Metals and Palm Beach Shutters</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">6</td>
+    <td class="tg-0lax">Top Down</td>
+    <td class="tg-0lax">Top-Down (only) operation includes Duette/Applause Top-Down.</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">7</td>
+    <td class="tg-02ax">Top Down Bottom Up</td>
+    <td class="tg-0lax">Shades with Top-Down/Bottom-Up (TDBU) operation or stacking Duolite operation including Duette/Applause TDBU, Solera TDBU, Vignette TDBU, Provenance TDBU; Alustrao Woven Textureso Romans TDBU, Duette/Applause Duolite.</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">8</td>
+    <td class="tg-02ax">Duolite Lift</td>
+    <td class="tg-0lax">Shades with lift only Duolite operation eg. Vignette Duolite, Roller/Screen Duolite (not released).</td>
+  </tr>
+  <tr>
+    <td class="tg-baqh">9</td>
+    <td class="tg-02ax">Duolite Lift and Tilt 90°</td>
+    <td class="tg-0lax">Duolite lift operation plus 90° tilt operation. Includes: Silhouette Duolite.</td>
+  </tr>
+</tbody>
+</table>'''
+    }
 }
 
 def refresh() {
@@ -74,30 +157,39 @@ public handleEvent(shadeJson) {
     }
 
     if (shadeJson?.batteryStrength) {
-        def batteryPercent = (int)(shadeJson.batteryStrength * 100 / 255)
-        sendEvent(name: "battery", value: batteryPercent, , unit: "%")
+        def battVoltage = shadeJson.batteryStrength / 10
+        def batteryLevel = (int)((battVoltage / maxVoltage) * 100)
+        
+        state.batteryVoltage = battVoltage
+        descriptionText = "${device.displayName} battery is ${batteryLevel}%"
+        sendEvent([name: "battery", value: batteryLevel, unit: "%", descriptionText: descriptionText])
     }
 	
 	state.batteryStatus = shadeJson.batteryStatus;  // 0 = No Status Available, 1 = Low, 2 = Medium, 3 = High, 4 = Plugged In
-	state.shadeType = shadeJson.type;  // Need the shade types from Hunter Douglas so the right functions are used against the right shade type
+    state.capabilities = shadeJson.capabilities;
+    
+    device.updateDataValue("firmwareVersion", "${shadeJson.firmware.revision}.${shadeJson.firmware.subRevision}.${shadeJson.firmware.build}")
+    device.updateDataValue("shadeTypeID", "${shadeJson.type}")
 }
 
 def updatePosition(position, posKind) {
-    def intPosition = (int)(position * 100 / 65535)
+    def level = (int)(position * 100 / 65535)
     def eventName = (posKind == 1) ? "bottomPosition" : "topPosition"
-    if (logEnable) log.debug "sending event ${eventName} with value ${intPosition}"
+    if (logEnable) log.debug "sending event ${eventName} with value ${level}"
 
-    sendEvent(name: eventName, value: intPosition)
-    sendEvent(name: "level", value: intPosition)
+    sendEvent(name: eventName, value: level)
+    sendEvent(name: "level", value: level)
+    sendEvent(name: "position", value: level)
 
-    if (intPosition >= 99) {
-        stateName = 'open'
-    } else if (intPosition > 0) {
-        stateName = 'partially open'
-    } else {
-        stateName = 'closed'
-    }
-    sendEvent(name: 'windowShade', value: stateName)
+    if (level > 0 && level < 99) {
+		sendEvent(name: "windowShade", value: "partially open", displayed:true)
+	}
+	else if (level >= 99) {
+		sendEvent(name: "windowShade", value: "open", displayed:true)
+	}
+	else {
+		sendEvent(name: "windowShade", value: "closed", displayed:true)
+	}
 }
 
 // parse events into attributes
@@ -107,13 +199,20 @@ def parse(String description) {}
 def open() {
     if (logEnable) log.debug "Executing 'open'"
     
-    //TODO get a mapping of the different shade types and add to switch statement
-    switch (state.shadeType) {
-        case 6:    // Duette, Applause
-            parent.setPosition(device, [position: 100])
+    switch (state.capabilities) {
+        case 0:    // Bottom Up
+            parent.setPosition(device, [bottomPosition: 100])
+            break
+        case 3:    // Vertically oriented
+            parent.setPosition(device, [bottomPosition: 100])
+            break        
+        case 6:    // Top Down
+            parent.setPosition(device, [topPosition: 0])
+            break
+        case 7:    // Top Down Bottom Up
+            parent.setPosition(device, [bottomPosition: 100, topPosition: 0])
             break
         default:
-            parent.setPosition(device, [bottomPosition: 100, topPosition: 0])
             break
     }
 }
@@ -121,13 +220,20 @@ def open() {
 def close() {
     if (logEnable) log.debug "Executing 'close'"
     
-    //TODO get a mapping of the different shade types and add to switch statement
-    switch (state.shadeType) {
-        case 6:    // Duette, Applause
-            parent.setPosition(device, [position: 0])
+    switch (state.capabilities) {
+        case 0:    // Botton Up
+            parent.setPosition(device, [bottomPosition: 0])
+            break
+        case 3:    // Vertically oriented
+            parent.setPosition(device, [bottomPosition: 0])
+            break
+        case 6:    // Top Down
+            parent.setPosition(device, [topPosition: 100])
+            break
+        case 7:    // Top Down Bottom Up
+            parent.setPosition(device, [bottomPosition: 0, topPosition: 0])
             break
         default:
-            parent.setPosition(device, [bottomPosition: 0, topPosition: 0])
             break
     }
 }
