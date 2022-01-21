6fe4f7a0bf82003d6b7a8357e53ad969b826264a
Brian Ujvary <40494729+bujvary@users.noreply.github.com>
2022-01-21 12:24:02 -0900

    Version number update only

--- drivers/hunter-douglas-powerview-room.groovy
+++ drivers/hunter-douglas-powerview-room.groovy
@@ -15,6 +15,7 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  *  Change Log:
+ *    01/20/2022 v2.2.0 - Version number update only
  *    01/19/2022 v2.1.0 - Version number update only
  *    08/25/2020 v1.1 - Added scene capability so rooms could be controlled from dashboard
  *    05/10/2020 v1.0 - Initial release
