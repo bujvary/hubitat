d3b6cde152c88ccf581b6f7b6dd03b435d38a227
Brian Ujvary <40494729+bujvary@users.noreply.github.com>
2022-01-21 15:29:15 -0900

    Version number update only

--- drivers/hunter-douglas-powerview-repeater.groovy
+++ drivers/hunter-douglas-powerview-repeater.groovy
@@ -15,6 +15,7 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  *  Change Log:
+ *    01/21/2022 v2.3.0 - Version number update only
  *    01/20/2022 v2.2.0 - Version number update only
  *    01/19/2022 v2.1.0 - Version number update only
  *    10/08/2020 v1.2 - Modified to check for null value for brightness
