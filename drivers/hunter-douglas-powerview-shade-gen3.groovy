037ff4c28a4b804963a6e7b0b3a759d47144e2af
Brian Ujvary <40494729+bujvary@users.noreply.github.com>
2022-12-08 07:53:50 -0900

    Version number update only

--- drivers/hunter-douglas-powerview-shade-gen3.groovy
+++ drivers/hunter-douglas-powerview-shade-gen3.groovy
@@ -15,6 +15,7 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  *  Change Log:
+ *    12/08/2022 v0.7 - Version number update only
  *    12/08/2022 v0.6 - Version number update only
  *    10/07/2022 v0.5 - Added logic to convert shade type to an equivalent shade capabilities value
  *                    - Added logic to reinitialize the "Shade capability information" state on install
